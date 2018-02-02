6bd8d84667cf169ca2548351169a0f9fc767470a
Gabriel Fernandes <gfernandes410@gmail.com>
2018-02-02 00:16:29 -0200

    Deploy dia 01/02

--- php/registratreino.php
+++ php/registratreino.php
@@ -5,26 +5,22 @@
 # Chama a conexão com o banco
 include "connectdb.php";
 
+mysql_query($query); 
+
+echo  $query;
+
 $ipt_exercicio = $_POST['ipt_exercicio'];
-$ipt_series = $_POST['ipt_series'];
-$ipt_repeticoes = $_POST['ipt_repeticoes'];
-$ipt_carga = $_POST['ipt_carga'];
 $ipt_obs = $_POST['ipt_obs'];
 
 $query = "INSERT INTO exercicio (
 		  `exercicio`,
-		  `series`,
-		  `repeticoes`,
-		  `carga`,
 		  `obs`) VALUES ( '";
 
 $query = $query . $ipt_exercicio  . "','";
-$query = $query . $ipt_series . "','";
-$query = $query . $ipt_repeticoes . "','";
-$query = $query . $ipt_carga . "','";
 $query = $query . $ipt_obs . "')";
 
-echo $query;
+//echo $query;
 
 mysql_query($query); 
+
 ?>
